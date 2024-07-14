4c233ddec2b82bd34f3d31132ccc0f28f2964576
ahan-stark <ahanabubakar@gmail.com>
2024-07-14 19:30:37 +0530

    added custom hook to get and store the movie details with trailer

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,6 +8,7 @@ import { getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  apiKey: "AIzaSyB7bTaKdtlI6_DaHYlpf1ubNQX6nKAuHEU",
   authDomain: "netflix-c7dde.firebaseapp.com",
   projectId: "netflix-c7dde",
   storageBucket: "netflix-c7dde.appspot.com",
