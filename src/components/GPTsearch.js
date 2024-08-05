f468e592169267b8a6d59bb0017affd752b98ff1
ahan-stark <ahanabubakar@gmail.com>
2024-08-05 17:46:36 +0530

    removed scrollbar from homescreen

--- src/components/GPTsearch.js
+++ src/components/GPTsearch.js
@@ -4,14 +4,15 @@ import bg from "../assets/bg.jpg";
 
 const GPTsearch = () => {
   return (
-
+    <div className="">
       <div className="">
         <img
-          className=" w-screen h-[84em] object-cover sm:h-full -z-20 absolute"
+          className=" w-screen min-h-screen object-cover sm:h-full -z-20 absolute"
           src={bg}
           alt="logo"
         />
-    
+      </div>
+
       <div className="">
         <GPTSearchBar></GPTSearchBar>
       </div>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -7,7 +7,7 @@ const MovieList = (props) => {
       <div className="font-bold text-3xl text-red-600 pt-4 pb-4 ml-4">
         {title}
       </div>
-      <div className="flex overflow-x-scroll ml-5">
+      <div className="flex overflow-x-scroll ml-5 no-scrollbar">
         <div className="flex">
           {movies.results.map((movie) => {
             return <MovieCard key={movie.id} movie_id = {movie.id} poster_path={movie.poster_path} />;
